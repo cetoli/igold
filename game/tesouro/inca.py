2efc45727de9228bf433699a0f3506f1ce537ea9
carlotolla <carlotolla@gmail.com>
2021-08-18 13:32:35 -0300

    New ws message protocol <action argument>

--- game/tesouro/inca.py
+++ game/tesouro/inca.py
@@ -16,17 +16,39 @@
 #
 # Você deve ter recebido uma cópia da Licença Pública Geral GNU
 # junto com este programa, se não, veja em <http://www.gnu.org/licenses/>
+# SPDX-License-Identifier: GPL-3.0-or-later
+""" Jogo de Exploração do Tesouro Inca - Multiusuário.
 
-"""Módulo que realiza comunicação entre browsers via websocket.
-.. moduleauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
+.. codeauthor:: Carlo Oliveira <carlo@ufrj.br>
+
+    Classes neste módulo:
+        - :py:class:`WebSocket` Módulo de comunicação entre instâncias.
+        - :py:class:`Jogador`   Personagem principal do jogo.
+        - :py:class:`Inca`      Representa o jogo, montando o tabuleiro.
+
+Changelog
+---------
+.. versionadded::    21.08
+        Classe :py:class:`WebSocket` .
+        Classe :py:class:`Jogador` .
+        Classe :py:class:`Inca` .
+
+.. versionchanged::    21.08
+        FUNC: Vira as cartas.
+
+.. versionchanged::    21.08.a
+        FIX: Cartas de aposta funcionando, precisa estado.
+        FUNC: Novo protocolo de mensagem <action argument>
+
 """
+__version__ = "21.08.a"
 import json
 
 # noinspection PyUnresolvedReferences
 from browser import websocket
 # noinspection PyUnresolvedReferences
 from browser import ajax
-from tesouro.main import GUI, SPRITES, Sprite
+from tesouro.main import GUI, SPRITES, Sprite, CRDW
 
 
 # noinspection PyMethodMayBeStatic
@@ -57,12 +79,12 @@ class WebSocket:
         print("open(self)", evt)
 
     def send(self, msg):
-        print("open(self)", msg)
+        print("send(self)", msg)
         self.ws.send(msg)
 
 
 class Jogador(Sprite):
-    ME = "YOU ME".split()
+    ME = "HE ME".split()
     PL = "NO GO".split()
 
     def __init__(self, grupo, img="aposta", user="user_00", is_me=False):
@@ -71,24 +93,52 @@ class Jogador(Sprite):
         self.is_me = is_me
         self.user = user
         self.play = False
+        self.decidiu = False
         self.grupo = grupo
         self.do_mostra()
         self.vai = self.grupo.play_no
 
-    def do_mostra(self):
-        isme, play = self.ME[self.is_me], self.PL[self.play]
-        self.mostra(f"{self.user}:{isme}:{play}")
+    def turn_bet(self, face="decide", *_):
+        index = 1 if self.play else 0
+        self.play = False if face == "aposta" else self.play
+        self.decidiu = False if face == "aposta" else self.decidiu
+        img = SPRITES[face]
+        img["index"], img["tit"] = index, ""
+        self.reface(**img)
+        self.do_mostra()
 
-    def do_play(self, sense, do_fw=False):
+    # noinspection PyUnusedLocal
+    def reface(self, img, index, tit=""):
+        w, h = self.siz
+        self.img = img
+        self.siz = (w, h)
+        cw = -int(CRDW) if self.decidiu else -int(w/2)
+        print("reface user, w, h, cw, index ", self.user, w, h, cw, index)
+        self.pos = (cw*index, 0)
+
+    def do_mostra(self):
+        # print("isme, play = self.ME[", self.is_me, " self.PL[", self.play)
+        is_me, play = self.ME[self.is_me], self.PL[self.decidiu]
+        # print("isme, play = self.ME[", is_me, " self.PL[", play)
+        self.mostra(f"{self.user}:{is_me}:{play}")
+
+    def decide(self, sense):
+        self.decidiu = True
+        self.play = True if sense in "True" else False
+        print("def decide -> user, self.play, sense= ", self.user, self.play, sense)
+        self.do_mostra()
+
+    def do_play(self, sense):
         # print("self.play = ", sense)
         self.play = sense
-        self.grupo.play(self, sense, do_fw=do_fw)
+        self.grupo.play(sense)
         self.do_mostra()
 
 
 class Inca:
     def __init__(self):
-        self.players = []
+        self.explorers = {}
+        self.action = dict(decide=self.decide, explora=self.explora)
         self.face = "aposta"
         self.users = {}
         self.jogador: Jogador = Jogador(self)
@@ -97,15 +147,34 @@ class Inca:
         self.handlers = dict(ROOM_JOIN=self.me_join, USER_JOIN=self.you_join,
                              USER_LEAVE=self.you_leave, MESSAGE=self.message, )
 
-    def play(self, player, message, do_fw=True):
-        self.players.append(player)
-        self.ws.send(str(message)) if do_fw else None
+    def decide(self, player, message):
+        self.users[player].decide(message)
+        face = "decide"
+        decidiram = all(user.decidiu for user in self.users.values())
+        [user.turn_bet(face) for user in self.users.values()] if decidiram else None
+
+    def explorar(self, *_):
+        self.ws.send("explora True")
+
+    def explora(self, player, _):
+        self.explorers[player] = True
+        face = "aposta"
+        [user.turn_bet(face) for user in self.users.values()]
+
+    def play(self, message):
+        self.ws.send(f"decide {str(message)}")
+
+    # noinspection PyArgumentList
+    def parse_message(self, player, message):
+        action, argument = message.split()
+        self.action[action](player, argument)
 
     def message(self, data):
-        print("me_join", data)  # , data["user_id"])
-        oid = data["user_id"]
-        sense = data["msg"] == "True"
-        self.users[oid].do_play(sense)
+        print("message", data)  # , data["user_id"])
+        self.parse_message(data["user_id"], data["msg"])
+        # oid = data["user_id"]
+        # sense = data["msg"] == "True"
+        # self.users[oid].do_play(sense)
 
     def me_join(self, data):
         print("me_join", data)  # , data["user_id"])
@@ -139,10 +208,10 @@ class Inca:
         ajax.get(req, oncomplete=self.add_users)
 
     def play_go(self, *_):
-        self.jogador.do_play(True, do_fw=True)
+        self.jogador.do_play(True)
 
     def play_no(self, *_):
-        self.jogador.do_play(False, do_fw=True)
+        self.jogador.do_play(False)
 
     def inicia(self):
         GUI.inicia()
@@ -151,6 +220,7 @@ class Inca:
         self.ws.inicia()
         self.get_users()
         self.cenario.labirinto.vai = self.play_go
+        self.cenario.acampamento.vai = self.explorar
 
     def append_user(self, user, is_me=False):
         if user in self.users:
